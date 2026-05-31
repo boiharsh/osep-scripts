096e720013ab64db7b487ed7b309a0a1fa601d2d
boi <793harshsingh@gmail.com>
2026-05-31 18:32:46 -0400

    Adding update-hosts.sh script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # osep-scripts
-Scripts to easy my osep workflows
+Scripts to ease my osep workflows.
+
+This project is WIP and i'll keep posting new scripts here as I work through my course.
+
+## Scripts
+1. update-hosts.sh: wrote this quick script to take in a CIDR range and add the host name and the IP address to your /etc/hosts file.
---
+++ update-hosts.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+HOSTS_FILE="/etc/hosts"
+# TARGET_IP="192.168.120.1-254"
+
+while getopts "i:h" opt; do
+  case $opt in
+    i) TARGET_IP="$OPTARG"
+    ;;
+    h) echo "Usage: $0 [-i <ip_range>]"
+       exit 0
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+done
+
+if [ -z "$TARGET_IP" ]; then
+    echo "Usage: $0 [-i <ip_range>]"
+    exit 1
+fi
+
+TARGET_IP="$(echo "$TARGET_IP" | cut -d'.' -f1-3).1-254"
+
+function update_hosts() {
+    IP=$1
+    DOMAIN=$2
+
+    num=0
+
+    while IFS= read -r line; do
+        num=$((num+1))
+        shopt -s nocasematch
+        if [[ " ${line} " =~ " ${DOMAIN} " ]]; then
+            OLD_IP=$(echo $line | cut -d' ' -f1)
+            echo "[+] Updating ${OLD_IP} to ${IP} for domain: ${DOMAIN}"
+            echo "[*] sed -i \"${num}s|${OLD_IP}|${IP}|\" $HOSTS_FILE"
+            sudo sed -i "${num}s|${OLD_IP}|${IP}|" $HOSTS_FILE
+            return 0
+        fi
+        shopt -u nocasematch
+    done < $HOSTS_FILE
+
+    echo "[+] Adding ${IP} for domain: '$DOMAIN'"
+    if [ -s "$HOSTS_FILE" ] && [ "$(tail -c 1 "$HOSTS_FILE")" != "" ]; then
+        echo "" | sudo tee -a "$HOSTS_FILE"
+    fi
+    echo "$IP $DOMAIN" | sudo tee -a $HOSTS_FILE
+
+    return 0
+}
+
+echo "[i] Gathering hosts using smb."
+nxc smb "$TARGET_IP" | grep -i smb | awk '{split($14,n,"[:)]"); split($15,d,"[:)]"); print $2" "tolower(n[2])"."d[2]}' > /tmp/output.txt
+echo "[+] Done"
+echo "[i] Gathering hosts using winrm."
+nxc winrm "$TARGET_IP" | grep -i winrm | awk '{split($13,n,"[:)]"); split($14,d,"[:)]"); print $2" "tolower(n[2])"."d[2]}' >> /tmp/output.txt
+echo "[+] Done"
+
+echo "[i] Writing hosts to /etc/hosts file."
+cat /tmp/output.txt | sort -u > /tmp/fin.txt
+
+while IFS= read -r line; do
+    IP=$(echo $line | cut -d' ' -f1)
+    DOMAIN=$(echo $line | cut -d' ' -f2)
+    update_hosts $IP $DOMAIN
+done < /tmp/fin.txt
+echo "[+] Done"
+
+rm /tmp/output.txt /tmp/fin.txt
